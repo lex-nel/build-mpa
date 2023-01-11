e01f0f071b6d985f8adc710561be5a023b51a6ab
LEx Klimin (lex-nel) <lex.klimin@gmail.com>
2023-01-11 11:15:19 +0700

    chore: changed tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./src/**/*.{html,liquid,js,ts}"],
   theme: {
     extend: {},
   },
