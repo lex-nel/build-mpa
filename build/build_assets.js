510701587374fb782ce775d972a595fca4ae6119
Алексей Климин <lex.klimin@gmail.com>
2025-02-08 19:23:36 +0700

    chore: eslint upgrade

--- build/build_assets.js
+++ build/build_assets.js
@@ -1,9 +1,9 @@
-const fse = require("fs-extra");
-const path = require("path");
+const fse = require('fs-extra')
+const path = require('path')
 
 module.exports = function () {
-  const entryPath = path.join(__dirname, "../src/assets/");
-  const outPath = path.join(__dirname, "../dist/assets/");
+  const entryPath = path.join(__dirname, '../src/assets/')
+  const outPath = path.join(__dirname, '../dist/assets/')
 
-  return fse.copy(entryPath, outPath, { overwrite: true });
-};
+  return fse.copy(entryPath, outPath, { overwrite: true })
+}
