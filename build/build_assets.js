efa778acd5794b00d9b01c1cfe42e8213ab9b6fa
Алексей Климин <lex.klimin@gmail.com>
2025-02-09 16:55:06 +0700

    chore: delete package fs-extra

--- build/build_assets.js
+++ build/build_assets.js
@@ -1,9 +1,9 @@
-const fse = require('fs-extra')
+const fs = require('fs')
 const path = require('path')
 
 module.exports = function () {
   const entryPath = path.join(__dirname, '../src/assets/')
   const outPath = path.join(__dirname, '../dist/assets/')
 
-  return fse.copy(entryPath, outPath, { overwrite: true })
+  return fs.cp(entryPath, outPath, { force: true, recursive: true }, () => {})
 }
