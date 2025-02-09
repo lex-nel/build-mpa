ff30a01e68f070c7fb68555dda809ae111caa9d6
Алексей Климин <lex.klimin@gmail.com>
2025-02-09 15:49:37 +0700

    chore: replace deprecated option

--- build/lint_styles.js
+++ build/lint_styles.js
@@ -3,6 +3,6 @@ const stylelint = require('stylelint')
 module.exports = function () {
   stylelint
     .lint({ formatter: 'verbose', files: 'src/styles/**/*.css' })
-    .then(data => console.log(data.output))
+    .then(data => console.log(data.report))
     .catch(err => console.error(err.stack))
 }
